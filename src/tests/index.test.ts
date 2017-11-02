5e8cfdac456c4e10cf879219fb3350f0d7fb667e
ascoders <576625322@qq.com>
2017-11-02 20:56:45 +0800

    add test file

---
+++ src/tests/index.test.ts
@@ -0,0 +1,9 @@
+import test from 'ava'
+import * as React from 'react'
+import { create } from 'react-test-renderer'
+import { observable } from 'dob'
+
+
+test('no args with no error and run once', t => {
+  t.true(true)
+})
